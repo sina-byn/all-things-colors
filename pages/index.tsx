610c6e04e4de1da71fb48a94d2a99778589a2574
Sina Bayandorian <sina.sina.bayan@protonmail.com>
2022-12-14 19:29:44 +0330

    layout layers order issue fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <header className='h-[50px] sticky top-0 bg-gray-100 py-3'>
+      <header className='h-[50px] sticky top-0 z-50 bg-gray-100 py-3'>
         <div className='header-content max-w-[1440px] flex items-center justify-between px-6 -mt-0.5 mx-auto'>
           <h1 className='text-2xl font-bold overflow-hidden'>
             <span className='text-orange-red'>A</span>ll&nbsp;
